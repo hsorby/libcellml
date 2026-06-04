ce08be95e8385dc590a41f3abccff05eaab35eb9
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-06-04 13:17:28 +1200

    Walkback changes.

--- tests/analyser/analyser.cpp
+++ tests/analyser/analyser.cpp
@@ -28,11 +28,11 @@ TEST(Analyser, unlinkedUnitsInModel)
 }
 
 
-// TEST(Version, versionMatch)
-// {
-//     unsigned int version = libcellml::version();
-//     EXPECT_EQ(0x000603U, version);
+TEST(Version, versionMatch)
+{
+    unsigned int version = libcellml::version();
+    EXPECT_EQ(0x000603U, version);
 
-//     std::string versionString = libcellml::versionString();
-//     EXPECT_EQ("0.6.3", versionString);
-// }
+    std::string versionString = libcellml::versionString();
+    EXPECT_EQ("0.6.3", versionString);
+}
